b4a82a741ea07a576e1ccd928dd3759e2ab88878
Your Name <you@example.com>
2023-11-24 02:16:12 +0200

    add seminar 3

---
+++ work1_3/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 1: Задайте одномерный массив из 10 целых чисел от 1 до 100. Найдите количество элементов массива, значения которых лежат в отрезке [20,90].
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Создаем массив из 10 целых чисел
+        int[] array = new int[10];
+
+        // Заполняем массив случайными целыми числами от 1 до 100
+        Random random = new Random();
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(1, 101);
+        }
+
+        // Выводим массив на экран
+        Console.WriteLine("Массив:");
+        foreach (int number in array)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
+
+        // Находим количество элементов в отрезке [20, 90]
+        int count = 0;
+        foreach (int number in array)
+        {
+            if (number >= 20 && number <= 90)
+            {
+                count++;
+            }
+        }
+
+       
+        Console.WriteLine($"Количество элементов в отрезке [20, 90]: => {count}");
+
+        
+                Console.ReadKey();
+    }
+}
---
+++ work2_3/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 2: Задайте массив на 10 целых чисел. Напишите программу, которая определяет количество чётных чисел в массиве.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Создаем массив из 10 целых чисел
+        int[] array = new int[] { 5, 12, 8, 3, 17, 22, 14, 9, 6, 11 };
+
+        // Выводим массив на экран
+        Console.WriteLine("Массив:");
+        foreach (int number in array)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
+
+        // Находим количество чётных чисел в массиве
+        int evenCount = 0;
+        foreach (int number in array)
+        {
+            if (number % 2 == 0)
+            {
+                evenCount++;
+            }
+        }
+
+        // Выводим результат
+        Console.WriteLine($"Количество чётных чисел в массиве: => {evenCount}");
+
+        // Ждем нажатия клавиши, чтобы консольное окно не закрылось сразу
+        Console.ReadKey();
+    }
+}
+
